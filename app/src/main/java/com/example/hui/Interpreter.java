229fef48626193ac4003a9d2b057656af0f1b145
Tom Hall <109621538+tomWJHall@users.noreply.github.com>
2025-03-22 03:53:22 +0530

    Adding link between the alphabets and the spinner in Interpreter

--- app/src/main/java/com/example/hui/Interpreter.java
+++ app/src/main/java/com/example/hui/Interpreter.java
@@ -9,6 +9,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +28,8 @@ public class Interpreter extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_DISPLAY = "display";
     private String displayString;
+    private SharedViewModel viewModel;
+    private List<List<String>> alphabets = new ArrayList<List<String>>();
 
     public Interpreter() {
         // Required empty public constructor
@@ -58,6 +67,25 @@ public class Interpreter extends Fragment {
         sharedViewModel.getReceivedData().observe(getViewLifecycleOwner(), this::setBinaryDisplay);
 
         return inflater.inflate(R.layout.fragment_interpreter, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        alphabets = viewModel.getAlphabets().getValue();
+
+        Spinner selectAlphabet = view.findViewById(R.id.alphabetSpinner);
+        List<String> newOptions = alphabets.stream()
+                .map(alpha -> alpha.get(0))
+                .collect(Collectors.toList()); // Collect into a List
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(),
+                android.R.layout.simple_spinner_item,
+                newOptions);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectAlphabet.setAdapter(adapter); // Update the spinner
     }
 
     public void setBinaryDisplay(String binary) {
